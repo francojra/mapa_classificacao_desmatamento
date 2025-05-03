8f71184c0dbb539e71fec2e6c0a919e2078daefe
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-05-02 23:40:55 -0300

    Add files via upload

--- mapa_classificacao_desmatamento.R
+++ mapa_classificacao_desmatamento.R
@@ -17,3 +17,19 @@ library(RStoolbox)
 library(sf)
 library(sp)
 library(viridis)
+
+# Carregar os dados ------------------------------------------------------------------------------------------------------------------------
+
+## Você precisará de:
+
+## 1 - Imagem de satélite (por exemplo, Landsat ou Sentinel)
+## 2 - Dados de referência (shapefiles ou outros dados vetoriais)
+
+# Carregar imagem de satélite (substitua pelo seu caminho)
+img <- stack("imagem.tif")
+
+# Carregar shapefile de áreas desmatadas (se disponível)
+desmatamento <- readOGR("shapefile.shp")
+
+# Visualizar os dados
+plotRGB(img, r = 4, g = 3, b = 2, stretch = "lin") # Para Landsat (bandas 4,3,2)
