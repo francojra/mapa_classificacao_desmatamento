8f49d853bb474ebf61be443ae49875e924f3fb6a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-05-15 22:12:09 -0300

    Add files via upload

--- mapa_classificacao_desmatamento.R
+++ mapa_classificacao_desmatamento.R
@@ -1,26 +1,23 @@
 
-# Elaborando um Mapa de Classificação de Áreas Desmatadas com Corte Raso no R --------------------------------------------------------------
+# Elaborando um Mapa de Áreas Desmatadas no R ----------------------------------------------------------------------------------------------
 # Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
 # Data: 02/05/2025 -------------------------------------------------------------------------------------------------------------------------
 
 # Instalar e carregar os pacotes necessários -----------------------------------------------------------------------------------------------
 
 # Instalar pacotes (se necessário)
-install.packages(c("raster", "terra","tidyverse", "frair",
-                   "RStoolbox", "sf", "sp", "viridis"))
+install.packages(c("raster", "terra","tidyverse", 
+                   "RStoolbox", "sf", "sp"))
 
 # install.packages("rgdal")
 
 # Carregar pacotes
 library(raster)
 library(terra)
-# library(rgdal)
 library(tidyverse)
-library(frair)
 library(RStoolbox)
 library(sf)
 library(sp)
-library(viridis)
 
 # Carregar os dados ------------------------------------------------------------------------------------------------------------------------
 
@@ -40,23 +37,6 @@ desmatamento <- read_sf("EDITION_21665_21.shp")
 
 # Visualizar os dados
 raster::plotRGB(img, r = 1, g = 3, b = 2, stretch = "lin") # Para Landsat (bandas 4,3,2)
-
-library(geobr)
-
-biomas <- geobr::read_biomes()
-view(biomas)
-
-class(biomas)
-glimpse(biomas)
-
-caatinga <- biomas |>
-  filter(name_biome %in% c("Caatinga"))
-
-## Mapa bioma Caatinga
-
-ggplot() +
-  geom_sf(data = caatinga, fill = "transparent") +
-  theme_minimal()
 
 # Pré-processamento da imagem --------------------------------------------------------------------------------------------------------------
 
@@ -89,48 +69,3 @@ plot(ndvi, main = "NDVI")
 ### Índices mais próximos do verde escuro ou 1, representam vegetação
 ### com mais vigor.
 
-# Classificação supervisionada -------------------------------------------------------------------------------------------------------------
-
-## 1 - Criar áreas de treinamento (se não tiver shapefiles)
-
-# Criar polígonos de treinamento (execute no console interativamente)
-train_data <- readOGR("EDITION_21665_21.shp")
-
-# Se não tiver dados de treinamento, você pode criar manualmente:
-#train_data <- drawPoly(img_cropped) # Para cada classe
-
-## 2 - Extrair valores espectrais para treinamento
-
-# Extrair valores das bandas para as áreas de treinamento
-train_values <- extract(img_cropped, train_data)
-train_values <- do.call(rbind, train_values)
-
-# Criar vetor de classes (ajuste conforme suas classes)
-classes <- rep(c("Floresta", "Corte_Raso", "Outros"), 
-               sapply(train_data@polygons, function(x) nrow(x@coords)))
-
-# Combinar em dataframe
-training <- data.frame(class = as.factor(classes), train_values)
-
-## 3 - Treinar o modelo de classificação
-
-# Modelo Random Forest (pode usar outros como SVM)
-library(randomForest)
-model_rf <- randomForest(class ~ ., data = training)
-
-# Classificar a imagem
-classification <- predict(img_cropped, model_rf)
-plot(classification, main = "Classificação Supervisionada")
-
-# Visualização final -----------------------------------------------------------------------------------------------------------------------
-
-# Mapa temático
-ggplot() +
-  geom_raster(data = as.data.frame(classification_fct, xy = TRUE), 
-              aes(x = x, y = y, fill = as.factor(layer))) +
-  scale_fill_manual(values = c("darkgreen", "brown", "gray"),
-                    labels = c("Floresta", "Corte Raso", "Outros"),
-                    name = "Classes") +
-  ggtitle("Mapa de Classificação de Áreas Desmatadas") +
-  coord_equal() +
-  theme_minimal()
